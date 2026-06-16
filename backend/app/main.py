d40050187401ca5f7c09cc3eece325ad924c88ec
sujithravuri01234 <sujithravuri01234@gmail.com>
2026-06-16 12:08:18 +0530

    fix: allow all CORS origins on backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,12 +13,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:4173",
-        "http://127.0.0.1:4173",
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
